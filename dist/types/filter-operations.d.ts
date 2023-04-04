dd6ab345e61233c8fc6d6627875528100235ed20
Gabriel Silva <gabrieljsilva2301@gmail.com>
2023-04-03 23:50:12 -0300

    update graphql filter version

--- dist/types/filter-operations.d.ts
+++ dist/types/filter-operations.d.ts
@@ -1,4 +1,4 @@
 import { LogicalOperations } from './logical-operations';
 import { EqualityOperations } from './equality-operations';
-export declare type FilterOperations<T> = LogicalOperations<T> & EqualityOperations<T>;
+export type FilterOperations<T> = LogicalOperations<T> & EqualityOperations<T>;
 //# sourceMappingURL=filter-operations.d.ts.map
